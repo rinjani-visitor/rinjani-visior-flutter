77d863819e0e1f17b2949b979e3d16c36c6788c3
Magistra <magistraagis@gmail.com>
2023-09-18 14:20:13 +0800

    page/add-login

---
+++ lib/page/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rinjani_visitor/theme/theme.dart';
+
+class LoginPage extends StatelessWidget {
+const LoginPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: backgroundColor,
+    );
+  }
+}
