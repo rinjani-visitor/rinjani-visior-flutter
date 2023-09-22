078ac1993f8cbea5de90255f2286c8e0cdaf7d91
Magistra <magistraagis@gmail.com>
2023-09-22 15:00:33 +0800

    feature/add-search

---
+++ lib/page/search_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rinjani_visitor/theme/theme.dart';
+import 'package:rinjani_visitor/widget/search_delegate.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Text('search'),
+    );
+  }
+}
