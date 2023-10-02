54ddcd97ea226268b0298da5cf8f4ec20c727969
Magistra <magistraagis@gmail.com>
2023-10-02 15:35:04 +0800

    fix/ add avatar

--- lib/page/account_page.dart
+++ lib/page/account_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/cupertino.dart';
+import 'package:rinjani_visitor/theme/theme.dart';
 
 class AccountPage extends StatelessWidget {
-const AccountPage({ Key? key }) : super(key: key);
+  const AccountPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Container(
-      child: Text('this is event'),
-    );
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: Text(
+            'Profile',
+            style: blackTextStyle.copyWith(fontSize: 32, fontWeight: semibold),
+          ),
+        ),
+        child: SafeArea(
+            child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration:
+                      BoxDecoration(shape: BoxShape.circle, color: lightGray),
+                  child: Center(child: Text('h')),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'Username',
+                  style:
+                      blackTextStyle.copyWith(fontSize: 24, fontWeight: bold),
+                )
+              ],
+            ),
+          ),
+        )));
   }
 }
