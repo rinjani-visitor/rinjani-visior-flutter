1bb7e303100cc19e3646da271e0bb7f512423325
Magistra <magistraagis@gmail.com>
2023-10-02 12:51:25 +0800

    page/ add notification card

---
+++ lib/widget/notification_card_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rinjani_visitor/theme/theme.dart';
+
+class NotificationCardWidget extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  const NotificationCardWidget({Key? key, required this.title, required this.subtitle, }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      child: CupertinoListTile(
+        onTap: (){},
+        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        title: Text(
+          '$title',
+          style: blackTextStyle.copyWith(fontSize: 16, fontWeight: semibold),
+        ),
+        subtitle: Text(
+            '$subtitle'),
+        backgroundColor: whiteColor,
+        trailing: Icon(
+          Icons.chevron_right,
+          color: lightGray,
+          size: 32,
+        ),
+      ),
+    );
+  }
+}
