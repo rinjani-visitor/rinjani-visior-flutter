76e4d0f4631d022aa50bfc69780e420c32abdd59
Magistra <magistraagis@gmail.com>
2023-10-03 12:05:02 +0800

    fix/add routing to notif detail

--- lib/widget/notification_card_widget.dart
+++ lib/widget/notification_card_widget.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rinjani_visitor/page/notification_detail_page.dart';
 import 'package:rinjani_visitor/theme/theme.dart';
 
 class NotificationCardWidget extends StatelessWidget {
   final String title;
   final String subtitle;
-  const NotificationCardWidget({Key? key, required this.title, required this.subtitle, }) : super(key: key);
+  const NotificationCardWidget({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 8),
       child: CupertinoListTile(
-        onTap: (){},
+        onTap: () {
+          Navigator.push(
+              context,
+              CupertinoPageRoute(
+                  builder: (context) => NotificationDetailPage()));
+        },
         padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
         title: Text(
           '$title',
           style: blackTextStyle.copyWith(fontSize: 16, fontWeight: semibold),
         ),
-        subtitle: Text(
-            '$subtitle'),
+        subtitle: Text('$subtitle'),
         backgroundColor: whiteColor,
         trailing: Icon(
           Icons.chevron_right,
