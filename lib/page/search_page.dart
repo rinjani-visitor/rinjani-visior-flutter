b00d9e7b23caa33c718e8f84bc6b62c179d8c10b
Magistra <magistraagis@gmail.com>
2023-10-10 16:36:16 +0800

    feature/ add auto complete on searchbar

--- lib/page/search_page.dart
+++ lib/page/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rinjani_visitor/theme/theme.dart';
+import 'package:rinjani_visitor/widget/auto_search.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -10,10 +11,11 @@ class SearchPage extends StatelessWidget {
     return CupertinoPageScaffold(
       backgroundColor: backgroundColor,
       navigationBar: const CupertinoNavigationBar(
+        automaticallyImplyLeading: false,
         padding: EdgeInsetsDirectional.only(bottom: 8),
-        middle: CupertinoSearchTextField(),
+        middle: const AutoSearch(),
       ),
-      child: const Center(child: Text('search')),
+      child: const Center(child: Text('No Content')),
     );
   }
 }
