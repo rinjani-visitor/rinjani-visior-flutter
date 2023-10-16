528e7a2c19740c97eb0d6882ffafb96e92c37e95
Agung Adhinata <adhi0asta@gmail.com>
2023-10-16 21:04:25 +0800

    feat: extend input text capability, add validator

--- lib/widget/input_field.dart
+++ lib/widget/input_field.dart
@@ -2,8 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:rinjani_visitor/theme/theme.dart';
 
 class InputField extends StatelessWidget {
-  final String label;
+  final String? label;
   final String? placeholder;
+  final String? errorText;
   final TextInputType? keyboardType;
   final TextEditingController? controller;
   final void Function(String value)? onChanged;
@@ -11,13 +12,14 @@ class InputField extends StatelessWidget {
   final bool secureText;
   const InputField(
       {Key? key,
-      required this.label,
+      this.label,
       this.secureText = false,
       this.placeholder,
       this.controller,
       this.onChanged,
       this.keyboardType,
-      this.onTap})
+      this.onTap,
+      this.errorText})
       : super(key: key);
 
   @override
@@ -32,22 +34,65 @@ class InputField extends StatelessWidget {
         const SizedBox(
           height: 4,
         ),
-        Container(
+        CupertinoTextField(
           decoration: BoxDecoration(
               color: mediumGray,
               borderRadius: BorderRadius.circular(smallRadius),
-              border: Border.all(color: lightGray)),
-          child: CupertinoTextFormFieldRow(
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            onChanged: onChanged,
-            controller: controller,
-            placeholder: placeholder,
-            obscureText: secureText,
-            keyboardType: keyboardType,
-            onTap: onTap,
-          ),
+              border: Border.all(
+                color: lightGray,
+              )),
+          scrollPadding: const EdgeInsets.symmetric(vertical: 12.0),
+          onChanged: (value) {
+            if (onChanged != null) {
+              onChanged!(value);
+            }
+          },
+          controller: controller,
+          placeholder: placeholder,
+          obscureText: secureText,
+          keyboardType: keyboardType,
+          onTap: onTap,
+        ),
+        Text(
+          errorText ?? "",
+          style: redTextStyle.copyWith(fontSize: body3),
         )
       ],
     );
   }
+}
+
+class InputFormField extends FormField<String> {
+  InputFormField(
+      {super.key,
+      String label = "",
+      bool secureText = false,
+      String? placeholder,
+      TextEditingController? controller,
+      void Function(String value)? onChanged,
+      TextInputType? keyboardType,
+      void Function()? onTap,
+      void Function(String?)? onSaved,
+      String? Function(String?)? validator,
+      String initialValue = "",
+      AutovalidateMode autovalidateMode = AutovalidateMode.onUserInteraction})
+      : super(
+            onSaved: onSaved,
+            validator: validator,
+            initialValue: initialValue,
+            autovalidateMode: autovalidateMode,
+            builder: (FormFieldState<String> state) {
+              controller?.text = state.value ?? "";
+              return InputField(
+                controller: controller,
+                keyboardType: keyboardType,
+                label: label,
+                onTap: onTap,
+                onChanged: (value) {
+                  state.didChange(value);
+                },
+                placeholder: placeholder,
+                errorText: state.hasError ? state.errorText : "",
+              );
+            });
 }
