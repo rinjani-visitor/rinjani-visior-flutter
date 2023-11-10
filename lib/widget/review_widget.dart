4106a728873407ff941cb3444268ba1c246dc439
agung-adhinata <adhi0asta@gmail.com>
2023-11-10 11:14:02 +0800

    feat: create mock review widget

--- lib/widget/review_widget.dart
+++ lib/widget/review_widget.dart
@@ -2,6 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:rinjani_visitor/theme/theme.dart';
 import 'package:rinjani_visitor/widget/review_card_widget.dart';
 
+class ReviewWidgetMock extends StatelessWidget {
+  const ReviewWidgetMock({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ReviewWidget(
+      reviewChildren: [
+        ReviewCardWidget(
+            name: "Kevin",
+            createdTime: " weeks ago",
+            message: "This place is amazing, I love it"),
+        ReviewCardWidget(
+            name: "Kevin",
+            createdTime: " weeks ago",
+            message: "This place is amazing, I love it"),
+      ],
+    );
+  }
+}
+
 class ReviewWidget extends StatelessWidget {
   final List<Widget> reviewChildren;
   const ReviewWidget({Key? key, required this.reviewChildren})
@@ -14,10 +34,6 @@ class ReviewWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Reviews',
-            style: blackTextStyle.copyWith(fontSize: 20, fontWeight: semibold),
-          ),
           SizedBox(
             height: 8,
           ),
