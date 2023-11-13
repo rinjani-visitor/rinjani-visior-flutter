94ad1dc55b34a5293318896b11a9ad66838fda05
Agung Adhinata <adhi0asta@gmail.com>
2023-11-13 19:21:20 +0800

    page: add notification state

--- lib/page/notification_page.dart
+++ lib/page/notification_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rinjani_visitor/core/enums/booking_enum.dart';
 import 'package:rinjani_visitor/core/presentation/theme/theme.dart';
+import 'package:rinjani_visitor/features/notification/presentation/notification_riverpod.dart';
 import 'package:rinjani_visitor/widget/notification_card_widget.dart';
+import 'package:rinjani_visitor/widget/status.dart';
 
 const List<Map<String, dynamic>> dataMock = [
   {
@@ -24,13 +28,20 @@ const List<Map<String, dynamic>> dataMock = [
   }
 ];
 
-class NotificationPage extends StatelessWidget {
+class NotificationPage extends ConsumerStatefulWidget {
   const NotificationPage({super.key});
 
+  @override
+  ConsumerState<NotificationPage> createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends ConsumerState<NotificationPage> {
   //TODO: change this when backend is finished
   final data = dataMock;
+
   @override
   Widget build(BuildContext context) {
+    final state = ref.watch(notificationRiverpodProvider);
     return CupertinoPageScaffold(
         backgroundColor: backgroundColor,
         navigationBar: const CupertinoNavigationBar(
@@ -39,16 +50,41 @@ class NotificationPage extends StatelessWidget {
         child: SafeArea(
             child: Container(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return NotificationCardWidget(
-                title: data[index]['title'],
-                subtitle: data[index]['subtitle'],
-                status: data[index]['status'],
-              );
-            },
-          ),
+          child: RefreshIndicator.adaptive(
+              onRefresh: () async {
+                // await ref.read(notificationRiverpodProvider.notifier).refresh();
+                await Future.delayed(const Duration(seconds: 2));
+              },
+              child: state.when(
+                data: (data) {
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final current = data[index];
+                      return NotificationCardWidget(
+                        data: current,
+                        title: current.title,
+                        subtitle: current.description,
+                        status: current.status ?? StatusColor.waiting,
+                      );
+                    },
+                  );
+                },
+                error: (error, stackTrace) {
+                  return Center(
+                    child: Text("Error occured: ${error.toString()}"),
+                  );
+                },
+                loading: () {
+                  return ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) => const NotificationCardWidget(
+                        title: "",
+                        subtitle: "",
+                        status: StatusColor.waiting),
+                  );
+                },
+              )),
         )));
   }
 }
