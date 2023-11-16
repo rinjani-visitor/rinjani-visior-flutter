42bc317b627720366e896470d32c0af5743400d2
Agung Adhinata <adhi0asta@gmail.com>
2023-11-16 16:47:17 +0800

    create wishlist entity

---
+++ lib/features/wishlist/domain/entity/wishlist.dart
@@ -0,0 +1,8 @@
+class WishLishEntity {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  WishLishEntity(
+      {required this.id, required this.title, required this.imageUrl});
+}
