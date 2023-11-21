987962dbf28cc395ef81976671266ee3decac4bf
agung-adhinata <adhi0asta@gmail.com>
2023-11-21 12:00:06 +0800

    feat: shortened month name

--- lib/core/presentation/utils/internationalization.dart
+++ lib/core/presentation/utils/internationalization.dart
@@ -1,4 +1,4 @@
 import 'package:intl/intl.dart';
 
-final dateFormat = DateFormat(DateFormat.YEAR_MONTH_DAY);
+final dateFormat = DateFormat(DateFormat.YEAR_ABBR_MONTH_DAY);
 final timeFormat = DateFormat(DateFormat.HOUR24_MINUTE);
