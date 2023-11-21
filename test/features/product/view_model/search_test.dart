d489a4e0f014de4b0c219e11e41da3f67ea8106c
agung-adhinata <adhi0asta@gmail.com>
2023-11-21 10:51:34 +0800

    update: test mocks and deps

--- test/features/product/view_model/search_test.dart
+++ test/features/product/view_model/search_test.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:rinjani_visitor/features/product/data/product_repository_impl.dart';
 
+import 'search_test.mocks.dart';
+
+@GenerateMocks([ProductRepositoryImpl])
 void main() {
   group("ProductSearchViewModel", () {
     test("should fetch packages when build", () => null);
