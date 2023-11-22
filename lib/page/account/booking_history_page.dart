41b45976b32f3a36a258dac3a073cb1ec4800f88
agung-adhinata <adhi0asta@gmail.com>
2023-11-22 11:43:34 +0800

    feat: add write review status into history

--- lib/page/account/booking_history_page.dart
+++ lib/page/account/booking_history_page.dart
@@ -4,6 +4,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rinjani_visitor/core/presentation/theme/theme.dart';
 import 'package:rinjani_visitor/core/presentation/utils/internationalization.dart';
 import 'package:rinjani_visitor/features/booking_history/domain/entitiy/booking_history.dart';
+import 'package:rinjani_visitor/features/booking_history/domain/enum/history_status.dart';
+import 'package:rinjani_visitor/page/review/write_review_page.dart';
+import 'package:rinjani_visitor/widget/button/primary_button.dart';
 import 'package:rinjani_visitor/widget/status.dart';
 
 final List<BookingHistoryEntity> mockData = [
@@ -28,6 +31,8 @@ class BookingHistoryPage extends ConsumerStatefulWidget {
 }
 
 class _BookingHistoryPageState extends ConsumerState<BookingHistoryPage> {
+  void _bookingHistoryTapped(String id) {}
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -52,77 +57,133 @@ class _BookingHistoryPageState extends ConsumerState<BookingHistoryPage> {
               itemCount: mockData.length,
               itemBuilder: (context, index) {
                 final current = mockData[index];
-                return GestureDetector(
-                  onTap: () {
-                    // TODO: navigate to booking history detail page
-                  },
-                  child: Container(
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    padding: const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      color: CupertinoColors.white,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: const Image(
-                              image: AssetImage('assets/products/coffee.jpg')),
-                        ),
-                        const SizedBox(height: 12),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(current.title,
-                                    style: blackTextStyle.copyWith(
-                                        fontSize: heading5,
-                                        fontWeight: FontWeight.bold)),
-                                const SizedBox(height: 8),
-                                Status(
-                                    status: StatusColor.loading,
-                                    text: current.status)
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    const Icon(Icons.calendar_today_outlined,
-                                        size: 16, color: CupertinoColors.black),
-                                    const SizedBox(width: 4),
-                                    Text(dateFormat.format(current.dateTime),
-                                        style: blackTextStyle.copyWith(
-                                            fontSize: subtitle1)),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    const Icon(Icons.access_time_outlined,
-                                        size: 16, color: CupertinoColors.black),
-                                    const SizedBox(width: 4),
-                                    Text(timeFormat.format(current.dateTime),
-                                        style: blackTextStyle),
-                                  ],
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+                return BookingHistoryCard(
+                  historyStatus: HistoryStatus.finish,
+                  title: current.title,
+                  status: current.status,
+                  dateTime: current.dateTime,
+                  onTap: () => _bookingHistoryTapped(current.id),
+                  onReviewTapped: () => Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                          builder: (context) => const WriteReviewPage())),
                 );
               },
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class BookingHistoryCard extends StatelessWidget {
+  final String title;
+  final String status;
+  final DateTime dateTime;
+  final HistoryStatus? historyStatus;
+  final Function() onTap;
+  final Function() onReviewTapped;
+  const BookingHistoryCard({
+    super.key,
+    required this.title,
+    required this.status,
+    required this.dateTime,
+    required this.onTap,
+    this.historyStatus,
+    required this.onReviewTapped,
+  });
+
+  StatusColor _fromHistoryStatus(HistoryStatus? status) {
+    switch (status) {
+      case HistoryStatus.canceled:
+        return StatusColor.error;
+      case HistoryStatus.onJourney:
+        return StatusColor.loading;
+      case HistoryStatus.finish:
+        return StatusColor.success;
+      default:
+        return StatusColor.init;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () => onTap(),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: CupertinoColors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child:
+                  const Image(image: AssetImage('assets/products/coffee.jpg')),
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Tooltip(
+                      message: title,
+                      child: Text(title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: blackTextStyle.copyWith(
+                              fontSize: heading5, fontWeight: FontWeight.bold)),
+                    ),
+                    const SizedBox(height: 8),
+                    Status(
+                        status: _fromHistoryStatus(historyStatus), text: status)
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.calendar_today_outlined,
+                            size: 16, color: CupertinoColors.black),
+                        const SizedBox(width: 4),
+                        Text(dateFormat.format(dateTime),
+                            style:
+                                blackTextStyle.copyWith(fontSize: subtitle1)),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.access_time_outlined,
+                            size: 16, color: CupertinoColors.black),
+                        const SizedBox(width: 4),
+                        Text(timeFormat.format(dateTime),
+                            style: blackTextStyle),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: 12),
+            historyStatus == HistoryStatus.finish
+                ? PrimaryButton(
+                    child: Text(
+                      "Create Review",
+                      style: whiteTextStyle.copyWith(fontSize: body1),
+                    ),
+                    onPressed: () => onReviewTapped())
+                : Container()
+          ],
         ),
       ),
     );
