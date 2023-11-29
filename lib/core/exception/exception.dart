35f595701a212c87418ef5743eb6ffb643346f12
Agung Adhinata <adhi0asta@gmail.com>
2023-11-29 12:53:28 +0800

    feat: improve error catching

--- lib/core/exception/exception.dart
+++ lib/core/exception/exception.dart
@@ -1,5 +1,6 @@
 import 'dart:developer' as developer;
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 enum ExceptionType {
   /// The exception for a bad response from the api.
@@ -46,14 +47,19 @@ class ExtException implements Exception {
   final String? errorMessage;
   final int? code;
   final ExceptionType exceptionType;
-  ExtException(
-      {required this.exceptionType,
-      this.message,
-      this.errorMessage,
-      this.code});
+
+  ExtException({
+    required this.exceptionType,
+    this.message,
+    this.errorMessage,
+    this.code,
+  });
+
   @override
   String toString() {
-    return "$message";
+    developer.log("$message || $errorMessage", name: "ExtException.toString");
+    if (errorMessage == null || errorMessage!.isEmpty) return "$message";
+    return "$errorMessage";
   }
 
   /// utility function for converting various exception into [ExtException] class
@@ -61,55 +67,60 @@ class ExtException implements Exception {
   /// [ExtException] is a custom exception class that directly return exception
   /// message when convert using `toString` method. this class is really useful for
   /// create error message at flutter pages / screen using toast or equivalent
-  /// without `Exception:` text _shenaigans_.
+  /// without default "Exception:" text _shenaigans_.
   factory ExtException.fromDioException(Object exception) {
     developer.log("Raw Error Catched",
         name: "ExtException.fromDioException", error: exception);
     var message = "";
     try {
       if (exception is DioException) {
-        final String? errorMessage = exception.response?.data["errors"][0];
+        // parse errors from List<String> to "string, string2, etc"
+        final errors =
+            List<String>.from(exception.response?.data["errors"] ?? []);
+        final errorsFull = errors != null ? errors.join(", ") : "";
+        String errorMessage = errorsFull;
+
         var code = exception.response?.statusCode;
         ExceptionType exceptionType = ExceptionType.unrecognizedException;
         switch (exception.type) {
           case DioExceptionType.connectionTimeout:
-            message += "Connection Timeout, cant reach server";
+            errorMessage += "Connection Timeout, cant reach server";
             exceptionType = ExceptionType.connectTimeoutException;
             break;
           case DioExceptionType.receiveTimeout:
-            message += "Receive Timeout, it might be server problem";
+            errorMessage += "Receive Timeout, it might be server problem";
             exceptionType = ExceptionType.receiveTimeoutException;
             break;
           case DioExceptionType.sendTimeout:
-            message += "Send Timeout";
+            errorMessage += "Send Timeout";
             exceptionType = ExceptionType.sendTimeoutException;
             break;
           case DioExceptionType.connectionError:
-            message += "Connection Error, Device is Offline";
+            errorMessage += "Connection Error, Device is Offline";
             exceptionType = ExceptionType.socketException;
             break;
           case DioExceptionType.badResponse:
-            message =
+            errorMessage =
                 "Bad response, ${exception.response?.data["message"].toString()}";
             exceptionType = ExceptionType.badResponseException;
             break;
           case DioExceptionType.cancel:
-            message += "${exception.message}";
+            errorMessage += "${exception.message}";
             exceptionType = ExceptionType.cancelException;
             break;
           case DioExceptionType.badCertificate:
-            message += "Bad Certificate (?)";
+            errorMessage += "Bad Certificate (?)";
             exceptionType = ExceptionType.unrecognizedException;
             break;
           case DioExceptionType.unknown:
             if (exception.response?.headers.value("code") == null) {
-              message +=
+              errorMessage +=
                   exception.response?.data["message"].toString() ?? 'Unknown';
               exceptionType = ExceptionType.unrecognizedException;
             }
             break;
           default:
-            message += "${exception.response?.data["message"].toString()}";
+            errorMessage += "${exception.response?.data["message"].toString()}";
             exceptionType = ExceptionType.unrecognizedException;
         }
         return ExtException(
@@ -129,6 +140,7 @@ class ExtException implements Exception {
         exceptionType: ExceptionType.unrecognizedException,
         message: exception.toString());
   }
+
   factory ExtException.fromParsingException(Exception error) {
     return ExtException(
         exceptionType: ExceptionType.serializationException,
