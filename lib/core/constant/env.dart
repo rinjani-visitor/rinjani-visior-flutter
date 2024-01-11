ddc3e72372965bc013e9e3fe306bd3bf140a5280
Agung Adhinata <adhi0asta@gmail.com>
2024-01-11 15:33:21 +0800

    feat: remove env

--- lib/core/constant/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: '.env')
-final class Env {
-  @EnviedField(varName: 'IMGUR_CLIENT_ID')
-  static const String imgurClientID = _Env.imgurClientID;
-}
--- lib/main_test.dart
+++ lib/main_test.dart
@@ -11,7 +11,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:rinjani_visitor/core/presentation/widget/button/primary_button.dart';
-import 'package:rinjani_visitor/features/img_storage/presentation/view_model.dart';
 
 void main() {
   // Force google font to use offline fonts instead of re-downloading again
@@ -32,88 +31,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      localizationsDelegates: [
+    return CupertinoApp(
+      localizationsDelegates: const [
         GlobalCupertinoLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalMaterialLocalizations.delegate
       ],
-      theme: CupertinoThemeData(
+      theme: const CupertinoThemeData(
         brightness: Brightness.light,
       ),
-      home: DevPage(),
-    );
-  }
-}
-
-class DevPage extends ConsumerStatefulWidget {
-  const DevPage({super.key});
-
-  @override
-  ConsumerState<DevPage> createState() => _DevPageState();
-}
-
-class _DevPageState extends ConsumerState<DevPage> {
-  Future<void>? _loadingUpload;
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final imageUrl = ref.watch(imgStorageProvider);
-    return CupertinoPageScaffold(
-      child: SafeArea(
-        child: Column(
-          children: [
-            CachedNetworkImage(
-              imageUrl: imageUrl,
-              imageBuilder: (context, imageProvider) {
-                return Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: imageProvider,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
-              },
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-              placeholder: (context, url) => const CircularProgressIndicator(),
-            ),
-            PrimaryButton(
-              child: const Text("Upload Img"),
-              onPressed: () {
-                final picker = ImagePicker();
-                picker.pickImage(source: ImageSource.camera).then(
-                  (value) {
-                    if (value != null) {
-                      _loadingUpload = ref
-                          .read(imgStorageProvider.notifier)
-                          .uploadImage(File(value.path));
-                    }
-                  },
-                );
-              },
-            ),
-            FutureBuilder(
-              future: _loadingUpload,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return const Text("Done");
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Text("Loading");
-                }
-                return const Text("Initial");
-              },
-            ),
-          ],
-        ),
-      ),
+      home: Container(),
     );
   }
 }
